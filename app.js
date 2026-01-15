15b3e68c59fa4c14711dc6b793ce176cb06a17d4
mairapaulac <mairapaulac@hotmail.com>
2026-01-15 12:48:21 -0300

    feat: postagem page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import session from 'express-session';
+import mongoose from 'mongoose';
 import handlebars from 'express-handlebars';
 import { config } from 'dotenv';
 import router from './routes/adminRoutes.js';
 import connDB from './database.js';
+import "./models/Postagem.js"
 
 const app = express();
 const adminRoutes = router;
+const Postagem = mongoose.model("postagens");
 config();
 connDB();
 
@@ -42,8 +45,32 @@ app.use('/bootstrap', express.static('node_modules/bootstrap/dist'))
 app.use('/admin', adminRoutes);
 
 
-app.get('/', (req, res) => {
-    res.render("index");
+app.get('/', async (req, res) => {
+    try {
+        const postagens = await Postagem.find().populate("categoria").sort({ data: "desc" });
+        res.render("index", { postagens: postagens }); //passando para nossas views
+    } catch (error) {
+       res.redirect("/404");
+    }
+})
+
+app.get("/postagem/:slug", async (req, res) => {
+    try {
+        const postagem = await Postagem.find({slug: req.params.slug});
+
+        if(postagem) 
+            res.render("postagem/index", { postagem: postagem });
+        
+
+    } catch (error) {
+        //add flash message pra erros
+        res.redirect("/");
+    }
+    
+})
+
+app.get('/404', (req, res) => {
+    res.render("404");
 })
 
 
